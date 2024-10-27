c118528d537268b264575e134d7e1bd60f8a8a38
snenenenenenene <sennebels@gmail.com>
2024-10-27 19:20:08 +0100

    removed compilation error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
 const handler = NextAuth(authOptions);
-
+// @ts-ignore
 export { handler as GET, handler as POST, authOptions };
