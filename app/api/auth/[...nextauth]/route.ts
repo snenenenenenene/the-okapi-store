8069ea911279e814299f3c128187964c6653ee44
snenenenenenene <sennebels@gmail.com>
2024-10-27 19:25:28 +0100

    removed unused export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,4 @@ import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
 const handler = NextAuth(authOptions);
-// @ts-ignore
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
