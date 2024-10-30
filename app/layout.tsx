0735cdd6222ea014bc954519c0fe22bbdb391333
snenenenenenene <sennebels@gmail.com>
2024-10-30 13:03:31 +0100

    Refactor layout.tsx to include Analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Providers from "./components/providers";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 import { WebsiteSchema, OrganizationSchema } from './components/seo';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = defaultMetadata;
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" data-theme="okapilight">
+      <Analytics />
       <head>
         <WebsiteSchema siteUrl={siteUrl} />
         <OrganizationSchema siteUrl={siteUrl} />
