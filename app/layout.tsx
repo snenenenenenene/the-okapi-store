6c774c18b3ced217c57facdebe826b19a8bf5c42
snenenenenenene <sennebels@gmail.com>
2024-11-17 15:35:59 +0100

    Refactor RootLayout: replace loading fallback with LoadingSpinner for improved user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,65 +1,31 @@
 // app/layout.tsx
 import { Analytics } from "@vercel/analytics/react";
-import { headers } from 'next/headers';
 import { Suspense } from 'react';
 import AnalyticsProvider from "./components/analytics/analytics-provider";
 import { Footer } from './components/footer';
 import { Header } from './components/header';
+import { LoadingSpinner } from './components/loadingStates';
 import Providers from "./components/providers";
 import "./globals.css";
 
-interface LayoutProps {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
-}
-
-export async function generateMetadata() {
-  const headersList = headers();
-  const domain = headersList.get('host') || 'https://the-okapi-webstore.vercel.app/';
-  const baseUrl = `https://${domain}`;
-
-  return {
-    metadataBase: new URL(baseUrl),
-    alternates: {
-      canonical: '/',
-    },
-    robots: {
-      index: true,
-      follow: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        'max-video-preview': -1,
-        'max-image-preview': 'large',
-        'max-snippet': -1,
-      },
-    },
-  };
-}
-
-export default function RootLayout({ children }: LayoutProps) {
+}) {
   return (
     <html lang="en" data-theme="okapilight">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="preload"
-          href="/fonts/your-main-font.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </head>
       <body className="antialiased bg-base-100 text-neutral flex flex-col min-h-screen">
         <Providers>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<LoadingSpinner />}>
             <Header />
           </Suspense>
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<LoadingSpinner />}>
+            <main className="flex-grow container mx-auto px-4 py-8">
               {children}
-            </Suspense>
-          </main>
-          <Suspense fallback={<div>Loading...</div>}>
+            </main>
+          </Suspense>
+          <Suspense fallback={<LoadingSpinner />}>
             <Footer />
           </Suspense>
           <Analytics />
